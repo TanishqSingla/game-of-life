6c0b4e41fcdcea3360a1967feb7a4aaac7a41968
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-07 16:20:58 +0530

    Adding a wrapper over index.js to handle async import

--- web/app.ts
+++ web/app.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('public/index.html');
+});
+
 app.listen(5000, () => {
   console.log("server is live at port 5000");
 });
