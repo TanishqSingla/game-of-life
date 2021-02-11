03374cf972b7b299cd0703bc1dc1200557ea7cd2
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-11 15:58:22 +0530

    Bug fixes
    Changed webpack, webpack-dev-server, webpack-cli and copy-webpack-plugin
    changed webpack config

--- web/public/index.js
+++ web/public/index.js
@@ -1,6 +1,6 @@
 import { Universe } from "wasm-game-of-life";
 
-const pre = document.getElementById("pre");
+const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
 const renderLoop = () => {
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,22 +2,11 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "/public/bootstrap.js",
+  entry: path.resolve(__dirname, "public/bootstrap.js"),
   output: {
     filename: "bootstrap.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [{ from: "public/index.html", to: "index.html" }],
-    }),
-  ],
-  experiments: {
-    outputModule: true,
-    syncWebAssembly: true,
-    topLevelAwait: true,
-    asyncWebAssembly: true,
-    layers: true,
-  },
+  plugins: [new CopyWebpackPlugin(["public/index.html"])],
 };
